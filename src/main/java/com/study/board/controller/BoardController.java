6556d1afc5b97c2e85df573a0e5063dd10098be9
SY97P <onetuks@naver.com>
2023-06-03 20:51:03 +0900

    1. DB 테이블 생성 2. 게시글 작성폼 생성

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BoardController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String main() {
-        return "Hello World";
+    @GetMapping("/board/write") //localhost:8090/board/write
+    public String boardWriteForm() {
+        // 어떤 view 파일을 보여줄 것인지 이름(확장자 제외)
+        return "boardwrite";
     }
 }
